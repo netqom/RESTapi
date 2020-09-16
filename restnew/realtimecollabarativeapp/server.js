f2472b302d3087e7fb3fc8ca391b54f2e9a6110a
webinfin <web.infin@gmail.com>
2020-09-16 15:22:47 +0530

    new folder uploaded named realtimecollabarativeapp regarding 4th point:Javascript

---
+++ restnew/realtimecollabarativeapp/server.js
@@ -0,0 +1,15 @@
+// initialize http server, socket.io and port number
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const port = 3000;
+http.listen(port, () => console.log(`server listening on port: ${port}`));
+io.on('connection', (socket) => {
+    console.log('connected');
+    socket.on('message', (evt) => {
+         console.log(evt);
+        socket.broadcast.emit('message', evt);
+    })
+});
+io.on('disconnect', (evt) => {
+     console.log('some people left');
+})
